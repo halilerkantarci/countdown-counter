cfe425641f042defe8289d1c5ca4c1bbfd134d62
halil <halilerk77@icloud.com>
2023-03-04 00:03:41 +0100

    finished

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const newYears = "1 Jan 2024";
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
 function countdown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minsEl.textContent = mins;
+  secondsEl.textContent = seconds;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(mins);
+  secondsEl.innerHTML = formatTime(seconds);
+
+  console.log(days, hours, mins, seconds);
   console.log(newYearsDate - currentDate);
 }
 
-// setInterval(countdown, 1000);
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+setInterval(countdown, 1000);
 
 countdown();
