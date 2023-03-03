18dd1c9a59ddc03fe24f3fd91d30f07dc3ce27e7
halil <halilerk77@icloud.com>
2023-03-03 23:51:07 +0100

    countdow-counter

---
+++ script.js
@@ -0,0 +1,11 @@
+const newYears = "1 Jan 2024";
+
+function countdown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+  console.log(newYearsDate - currentDate);
+}
+
+// setInterval(countdown, 1000);
+
+countdown();
